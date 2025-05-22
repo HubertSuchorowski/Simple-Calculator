94040567d50e548389a13b24eff23fdf60ca6a52
Hubert Suchorowski <hsuchorowski@gmail.com>
2025-05-22 21:38:18 +0200

    added the ability to perform simple calculations

--- Simple Calculator.cpp
+++ Simple Calculator.cpp
@@ -1,20 +1,73 @@
-﻿// Simple Calculator.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int choice;
+    double answer;
+    double number1;
+    double number2;
+    char kontynuuj;
+    std::cout << "Welcome in my Calculator\n";
+    do {
+        std::cout << "What operation do you want to use?\n";
+        std::cout << "1. Adding\n";
+        std::cout << "2. Subtraction\n";
+        std::cout << "3. Multiplication\n";
+        std::cout << "4. Divide\n";
+
+        std::cin >> choice;
+
+        switch (choice) {
+        case 1:
+            std::cout << "You chose adding\n";
+            std::cout << "State the first number: \n";
+            std::cin >> number1;
+            std::cout << "State the second number: \n";
+            std::cin >> number2; 
+            answer = number1 + number2;
+            std::cout << "Your answer is: " << answer << std::endl;
+            break;
+
+        case 2:
+            std::cout << "You chose subtraction\n";
+            std::cout << "State the first number: \n";
+            std::cin >> number1;
+            std::cout << "State the second number: \n";
+            std::cin >> number2;
+            answer = number1 - number2;
+            std::cout << "Your answer is: " << answer << std::endl;
+            break;
+        case 3:
+            std::cout << "You chose multiplication\n";
+            std::cout << "State the first number: \n";
+            std::cin >> number1;
+            std::cout << "State the second number: \n";
+            std::cin >> number2;
+            answer = number1 * number2;
+            std::cout << "Your answer is: " << answer << std::endl;
+            break;
+        case 4:
+            std::cout << "You chose divide\n";
+            std::cout << "State the first number: \n";
+            std::cin >> number1;
+            std::cout << "State the second number: \n";
+            std::cin >> number2;
+            if (number2 == 0) {
+                std::cout << "You can't divide by zero" << std::endl;
+            }
+            else {
+                answer = number1 / number2;
+                std::cout << "Your answer is: " << answer << std::endl;
+            }
+            break;
+        default:
+            std::cout << "You chose wrong option\n";
+            break;
+        }
+
+        std::cout << "Do you want to continue? (t/n): ";
+        std::cin >> kontynuuj;
+    } while (kontynuuj == 't' || kontynuuj == 'T');
 }
 
-// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
-// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
 
-// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
